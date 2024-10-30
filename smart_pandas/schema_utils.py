0bcf3ebe5ff023eb52ef44c53e80271cda14ffa3
Roland Webster <rolandwebster@gmail.com>
2024-10-29 21:35:41 -0700

    Remove strict default value

--- smart_pandas/schema_utils.py
+++ smart_pandas/schema_utils.py
@@ -1,7 +1,6 @@
 def default_df_schema_params() -> dict:
     return {
         "coerce": True,
-        "strict": False,  # want to allow columns to be missing if they're not created yet
         "unique_column_names": True,
         "add_missing_columns": False,  # make sure this is false to avoid adding calculated columns
     }
